7e99056a48502cd7a729a87d5a366a928c880dd8
Paul Stanton <paul@coffeepowered.co.uk>
2013-09-04 12:07:44 +0100

    Twig extension to set the active_tab global
    Based on the presence of $_GET['tab'] integer

--- lexicon/index.php
+++ lexicon/index.php
@@ -4,12 +4,15 @@
 	$templateDir = '../views';
 	
 	require_once 'vendor/autoload.php';
+	require_once 'extensions/url.php';
 
 	$loader = new Twig_Loader_Filesystem($templateDir);
 	$twig = new Twig_Environment($loader, array('debug' => true));
 
+	$twig->addExtension(new Pulsar_Twig_Extension_Url());
+
 	$template = $twig->loadTemplate('lexicon/main.html.twig');
 
-	echo $template->render(array());
+	print $template->render(array());
 
 ?>
