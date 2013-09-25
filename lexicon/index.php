34176d919f02971407174cae191cc41d2fed3c50
Paul Stanton <paul@coffeepowered.co.uk>
2013-09-25 11:59:18 +0100

    [FEATURE] Add relative time filter for twig
    Coverts a unix timestamp to a readable relative string like '2 days ago'

--- lexicon/index.php
+++ lexicon/index.php
@@ -4,12 +4,15 @@
 	$templateDir = '../views';
 	
 	require_once 'vendor/autoload.php';
-	require_once 'extensions/url.php';
 
 	$loader = new Twig_Loader_Filesystem($templateDir);
 	$twig = new Twig_Environment($loader, array('debug' => true));
 
-	$twig->addExtension(new Pulsar_Twig_Extension_Url());
+	require_once 'extensions/UrlParams.php';
+	$twig->addExtension(new Url_Params_Extension());
+	
+	require_once 'extensions/RelativeTime.php';
+	$twig->addExtension(new Relative_Time_Extension());	
 
 	$template = $twig->loadTemplate('lexicon/main.html.twig');
 
