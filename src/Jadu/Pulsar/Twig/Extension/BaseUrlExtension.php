3a17872e49de623ea66d82ad475b3ab589519ab5
Paul Stanton <paul@coffeepowered.co.uk>
2013-09-30 12:07:24 +0100

    Add markdown parser to Pulsar UI for documentation

---
+++ src/Jadu/Pulsar/Twig/Extension/BaseUrlExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Jadu\Pulsar\Twig\Extension;
+
+/**
+ * Base URL
+ */
+class BaseUrlExtension extends \Twig_Extension
+{
+
+    public function getName() 
+    {
+        return 'base_url_extension';
+    }
+
+    public function getGlobals()
+    {
+    	return array(
+            'base_url' => $this->getBaseUrl()
+        );
+    }
+
+    public function getBaseUrl()
+    {
+        return "http://" . $_SERVER['HTTP_HOST'] . '/';
+    }
+
+}
