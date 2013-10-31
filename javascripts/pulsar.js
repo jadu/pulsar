05119fdeaafea846c4fd6e437975b46d7666ba84
Paul Stanton <paul.stanton@jadu.net>
2013-10-31 12:20:06 +0000

    Fix tab hover behaviour in IE7

--- javascripts/pulsar.js
+++ javascripts/pulsar.js
@@ -44,27 +44,7 @@ require(['jquery'], function() {
                     $summary.hide();
                 }
             });
-        });
-
-        require(['daterange'], function() {
-            $('[data-daterange]').daterangepicker(
-                {
-                  ranges: {
-                     'Today': [moment(), moment()],
-                     'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
-                     'Last 7 Days': [moment().subtract('days', 6), moment()],
-                     'Last 30 Days': [moment().subtract('days', 29), moment()],
-                     'This Month': [moment().startOf('month'), moment().endOf('month')],
-                     'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
-                  },
-                  startDate: moment().subtract('days', 29),
-                  endDate: moment()
-                },
-                function(start, end) {
-                    $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-                }
-            );
-        });
+        });        
 
     });
 });
