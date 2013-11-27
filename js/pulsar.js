44f60b0bc2bf35a20bac21032b6f19261d19e05a
Paul Stanton <paul.stanton@jadu.net>
2013-11-27 13:10:02 +0000

    Init charts by targeting the last-appended widget Start capturing dashboard state so they can be saved

--- js/pulsar.js
+++ js/pulsar.js
@@ -10,23 +10,6 @@ define(['jquery'], function() {
 
             var widget_content = '';
 
-            // $('.tray__widgets .widget').draggable({
-            //   connectToSortable: '.dashboard',
-            //   helper: 'clone',
-            //   revert: 'invalid',
-
-            //   // Fetch widget content when dragging starts
-            //   start: function(event, ui) {
-            //     var widget = $(ui.helper.context).data('widget');
-            //     widget_content = '';
-            //     $.ajax({
-            //       url: '/views/widgets/' + widget + '/index.php'
-            //     }).done(function(data) {
-            //       widget_content = data;
-            //     });
-            //   }
-            // });
-
             $('.tray__detail .widget').draggable({
               connectToSortable: '.dashboard',
               helper: 'clone',
@@ -51,16 +34,30 @@ define(['jquery'], function() {
                 revert: 100,
                 tolerance: 'pointer',
                 // Populate widget content when widget is dropped into dashboard
-                receive: function(event, ui) {
+                receive: function(e, ui) {
                     var $this = $(this);
 
                     // If the fetch started by the dragging event hasn't finished, keep checking for the response...
                     isFetched();
+
                     function isFetched() {
                         if (widget_content != '') {
-                            
-                            // Populate the widget
-                            $this.data().uiSortable.currentItem.html(widget_content);
+
+                            var widget = $this.data().uiSortable.currentItem;
+                            var sender = ui.sender;
+
+                            widget.html(widget_content) // Populate widget content
+                                  .uniqueId();          // Attach unique identifier
+
+                            // Reset last-appended flag
+                            $('.dashboard [data-last-appended=true]').not(widget)
+                                .attr('data-last-appended', 'false');
+
+                            // Populate data attributes in new widget instance
+                            widget.data('widget', sender.data('widget'))
+                                  .data('widget-title', sender.data('widget-title'))
+                                  .data('widget-description', sender.data('widget-description'))
+                                  .data('last-appended', 'true');
 
                             // Tidy up after ourselves
                             widget_content = '';
@@ -70,9 +67,33 @@ define(['jquery'], function() {
                             setTimeout(isFetched, 50);
                         }
                     }
+                },
+                stop: function(e, ui) {
+
+                    var widget_state = [];
+
+                    // Grab the widget states
+                    $.map($(this).find('.widget'), function(el) {
+                        widget_state.push({
+                            guid:   "widget_guid",
+                            id:     $(el).attr('id'),
+                            name:   $(el).data('widget'),
+                            settings: [{
+                                name: "test_name",
+                                value: "test_value"
+                            }]
+                        });
+                    });
+
+                    // Create the json object that stores dashboard state
+                    var Dashboard = {
+                        title: "My Dashboard",
+                        widgets: widget_state
+                    };
+
+                    // console.log(JSON.stringify(Dashboard));
                 }
             });
-
 
             $('.dashboard').on('click', '.widget [data-widget-action=remove]', function() {
                 $(this).closest('.widget').remove();
@@ -85,8 +106,6 @@ define(['jquery'], function() {
                 $('.widget__title').text($this.data('widget-title'));
                 $('.widget__description').text($this.data('widget-description'));
                 $('.widget__price').text($this.data('widget-price'));
-
-console.log($this.data('widget'));
 
                 $('.tray__detail .widget').data('widget', $this.data('widget'))
                     .data('widget-title', $this.data('widget-title'))
