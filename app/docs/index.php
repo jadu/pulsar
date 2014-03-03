3549c5ff9d0d4b7e7bfbcf2e4f4e3857180b9d4b
Paul Stanton <paul.stanton@jadu.net>
2014-03-03 12:16:23 +0000

    Move /docs and /lexicon to /apps directory
    Cleans up the structure somewhat

--- app/docs/index.php
+++ app/docs/index.php
@@ -1,15 +1,18 @@
 <?php
 
-$baseDir = '../';
-$templateDir = '../views';
+$baseDir = '../../';
+$templateDir = $baseDir . 'views';
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/' . $baseDir . 'vendor/autoload.php';
+require_once __DIR__ . '/functions.php';
 
+use dflydev\markdown\MarkdownExtraParser;
 use Jadu\Pulsar\Twig\Extension\ConfigExtension;
 use Jadu\Pulsar\Twig\Extension\RelativeTimeExtension;
 use Jadu\Pulsar\Twig\Extension\UrlParamsExtension;
 use Jadu\Pulsar\Twig\Extension\TabsExtension;
 
+$markdownParser = new MarkdownExtraParser();
 $loader = new Twig_Loader_Filesystem($templateDir);
 $twig = new Twig_Environment($loader, array('debug' => true));
 
@@ -19,20 +22,22 @@ $twig->addExtension(new UrlParamsExtension($_GET));
 $twig->addExtension(new TabsExtension());
 $twig->addExtension(new Twig_Extension_Debug());
 
-$template = $twig->loadTemplate('lexicon/main.html.twig');
+$template = $twig->loadTemplate('docs/main.html.twig');
+
+$options = get_options();
+$tree = get_tree($options['docs_path'], $base_url);
+$homepage_url = homepage_url($tree);
+$docs_url = docs_url($tree);
+$page = load_page($tree, $markdownParser);
 
 $breadcrumb = array(
     'Pulsar' => '/',
-    'Documentation' => '/docs',
-    'Lexicon' => null
-    );
-
-$flashMessage = array(
-    'type' => 'success',
-    'message' => 'This is a flash message, it lets users know that something happened. <a href="#tab_4" data-toggle="tab">read more</a>.'
-    );
+    'Documentation' => null 
+);
 
 print $template->render(array(
     'breadcrumb' => $breadcrumb,
-    'flash_message' => $flashMessage
-    ));
+    'tabs' => build_nav($tree),
+    'content' => $page['html']
+    )
+);
--- app/lexicon/index.php
+++ app/lexicon/index.php
@@ -1,18 +1,15 @@
 <?php
 
-$baseDir = '../';
+$baseDir = '../../';
 $templateDir = $baseDir . 'views';
 
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../docs/functions.php';
+require_once __DIR__ . '/' . $baseDir . 'vendor/autoload.php';
 
-use dflydev\markdown\MarkdownExtraParser;
 use Jadu\Pulsar\Twig\Extension\ConfigExtension;
 use Jadu\Pulsar\Twig\Extension\RelativeTimeExtension;
 use Jadu\Pulsar\Twig\Extension\UrlParamsExtension;
 use Jadu\Pulsar\Twig\Extension\TabsExtension;
 
-$markdownParser = new MarkdownExtraParser();
 $loader = new Twig_Loader_Filesystem($templateDir);
 $twig = new Twig_Environment($loader, array('debug' => true));
 
@@ -22,22 +19,20 @@ $twig->addExtension(new UrlParamsExtension($_GET));
 $twig->addExtension(new TabsExtension());
 $twig->addExtension(new Twig_Extension_Debug());
 
-$template = $twig->loadTemplate('docs/main.html.twig');
-
-$options = get_options();
-$tree = get_tree($options['docs_path'], $base_url);
-$homepage_url = homepage_url($tree);
-$docs_url = docs_url($tree);
-$page = load_page($tree, $markdownParser);
+$template = $twig->loadTemplate('lexicon/main.html.twig');
 
 $breadcrumb = array(
     'Pulsar' => '/',
-    'Documentation' => null 
-);
+    'Documentation' => '/docs',
+    'Lexicon' => null
+    );
+
+$flashMessage = array(
+    'type' => 'success',
+    'message' => 'This is a flash message, it lets users know that something happened. <a href="#tab_4" data-toggle="tab">read more</a>.'
+    );
 
 print $template->render(array(
     'breadcrumb' => $breadcrumb,
-    'tabs' => build_nav($tree),
-    'content' => $page['html']
-    )
-);
+    'flash_message' => $flashMessage
+    ));
