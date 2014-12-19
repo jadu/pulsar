9f17b861e7ea04cc288eae88e41584f6b41d4e27
Paul Stanton <paul.stanton@jadu.net>
2014-12-19 10:59:27 +0000

    uploader javascript

--- js/uploader.js
+++ js/uploader.js
@@ -1,58 +1,210 @@
-/*global define, setTimeout, window */
+/*global define */
 define([
     'jquery'
 ], function (
     $
 ) {
-  'use strict';
+    'use strict';
 
-  (function (defaults, $, window, document, undefined) {
+    var defaults = {
+            mainSelector: '.uploader',
+            itemContainer: '.block-list',
+            itemSelector: '.block-list__item',
+            browseButtonSelector: '[data-action=browse]',
+            fileInputSelector: '[type=file]',
+            labelDefaultValue: 'No file selected',
+            labelSelector: '.uploader__label',
+            removeButtonSelector: '[data-action=remove]',
+            removeButtonMarkup: '<button type="button" class="remove-button" data-action="remove" data-toggle="tooltips" data-placement=right title="Remove this item" data-action=remove><i class="icon-remove-sign"></i></button>',
+            removeAllButtonSelector: '[data-action=remove-all]',
+            removeAllButtonMarkup: '<button type="button" class="btn btn--naked" data-action="remove-all"><i class="icon-remove-sign"></i> Remove all</button>',
+            addAnotherButtonSelector: '[data-action=add-another]',
+            addAnotherButtonMarkup: '<button type="button" class="btn" data-action="add-another">Add Another</button>',
+        };
 
-    $.extend({
-
-      pluginSetup: function (options) {
-        return $.extend(defaults, options);
-      }
-
-    }).fn.extend({
-
-      uploader: function (options) {
-        var _this = $(this);
-
+    $.fn.uploader = function (options) {
         options = $.extend({}, defaults, options);
 
-        return _this.each(function () {
-          _this.setUp(this, options);
+        return this.each(function () {
+            var $this = $(this),
+                $fileInput = $this.find(options.fileInputSelector),
+                $browseButton = $this.find($(options.browseButtonSelector)),
+                $defaultRow = $this.find(options.itemSelector).clone();
+
+
+            // Add File button
+            $browseButton.on('click', function () {
+                $(this).parents(options.mainSelector).find(options.fileInputSelector).trigger('click');
+            });
+
+
+            // Remove item icon
+            $this.on('click', options.removeButtonSelector, function () {
+              removeRow($(this).parent(options.itemSelector))
+            });
+
+
+            // Remove all button
+            $this.on('click', options.removeAllButtonSelector, function () {
+              var $uploader = $(this).parents(options.mainSelector),
+                  $rows = $uploader.find(options.itemSelector);
+
+              $(function () {
+                  (function next(index) {
+                      if (index >= 0) {
+                          removeRow($rows.eq(index), function () {
+                              next(index - 1);
+                          });
+                      }
+                  }($rows.length - 1));
+              });
+
+              $uploader.find(options.removeAllButtonSelector)
+                .fadeOut(250, function() {
+                  $(this).remove();
+                });
+            });
+
+
+            // Watch for changes to the file input
+            $this.on('change', options.fileInputSelector, function () {
+                var $row = $(this).parents(options.itemSelector),
+                    $label = $row.find(options.labelSelector),
+                    $uploader = $row.parents(options.mainSelector);
+
+                $row.addClass('has-changed fade');
+
+                switch($uploader.data('uploader-type')) {
+                case 'detailed':
+                  var $metadata = $row.find('.metadata'),
+                      $nameField = $row.find('.uploader__name');
+
+                  $metadata.removeClass('hide');
+                  $nameField.removeClass('hide');
+
+                  $metadata.find('.metadata__value').text($(this).val());
+
+                  $label.hide();
+
+                  $('.uploader__name').show();
+                default:
+                  $label.text($(this).val());
+                }
+
+                $row.find(options.removeButtonSelector).fadeTo(250, 1);
+
+                // Hide the Add File button
+                $uploader.find(options.browseButtonSelector).hide();
+
+                // Add the Add Another button
+                if (!$(options.addAnotherButtonSelector).length) {
+                  $(options.addAnotherButtonMarkup).insertAfter($(options.itemContainer, $this));
+                }
+
+                // Add the remove button
+                if (!$this.find(options.removeButtonSelector).length) {
+                  $(options.removeButtonMarkup).insertBefore($label.parent());
+                }
+
+                // Remove all?
+                if (($row.siblings().length > 0) && !$uploader.find(options.removeAllButtonSelector).length) {
+                  $uploader.append(options.removeAllButtonMarkup);
+                }
+
+            });
+
+
+            // Add Another item
+            $this.on('click', options.addAnotherButtonSelector, function () {
+                var $uploader = $(this).parents(options.mainSelector),
+                    $rows = $uploader.find(options.itemSelector),
+                    $newRow = $defaultRow
+                                .clone()
+                                .hide(),
+                    $fileInput = $newRow.find(options.fileInputSelector);
+
+                $fileInput.attr('id', 'uploader-' + $uploader.index() + '-file-' + ($rows.length + 1));
+
+                $newRow.appendTo($this.find(options.itemContainer))
+                  .slideDown(250, function() {
+                    var _this = $(this);
+
+                    // Add the remove button
+                    if (!_this.find(options.removeButtonSelector).length) {
+                      $(options.removeButtonMarkup).prependTo(_this);
+                    }
+
+                    _this.find(options.removeButtonSelector).fadeTo(250, 1);
+                    $(options.addAnotherButtonSelector).remove();
+                    $(options.browseButtonSelector).show();
+                });
+
+                $fileInput.trigger('click');
+
+            });
+
+
+            // Slide a row away and
+            function removeRow(row, callback) {
+              var $label = $this.find(options.labelSelector),
+                  $fileInput = row.find(options.fileInputSelector),
+                  $uploader = row.parents(options.mainSelector);
+
+              row.removeClass('has-changed fade').addClass('has-danger fade');
+
+              if (row.siblings().length > 0) {
+
+                row.slideUp(250, function() {
+
+                  if (row.siblings().length === 1) {
+                    $uploader.find(options.removeAllButtonSelector)
+                      .fadeOut(250, function() {
+                        $(this).remove();
+                      });
+                  }
+
+                  clearRow(row);
+                  $(this).remove();
+
+                  if (typeof callback == 'function') {
+                    callback.call(this);
+                  }
+                });
+
+              } else {
+                clearRow(row);
+              }
+            }
+
+            // Clear a row's contents back to it's initial state
+            function clearRow(row) {
+              var $label = $this.find(options.labelSelector),
+                  $fileInput = row.find(options.fileInputSelector),
+                  $uploader = row.parents(options.mainSelector);
+
+              switch($uploader.data('uploader-type')) {
+                case 'detailed':
+                  var $metadata = row.find('.metadata'),
+                      $nameField = row.find('.uploader__name');
+
+                  $metadata.addClass('hide');
+                  $nameField.addClass('hide');
+                  $metadata.find('.metadata__value').text('');
+                default:
+                  row.find(options.labelSelector).text(options.labelDefaultValue);
+              }
+
+              row.find(options.removeButtonSelector).fadeTo(250, 0);
+              $fileInput.wrap('<form>').parent('form').trigger('reset');
+              $fileInput.unwrap();
+
+              if (row.siblings().length < 1) {
+                $(options.addAnotherButtonSelector).remove();
+                $(options.browseButtonSelector).show();
+                $label.show();
+              }
+            }
+
         });
-      },
-
-      updateLabel: function (options) {
-
-      },
-
-      setUp: function (instance, options) {
-        var $instance = $(instance),
-            $fileInput = $instance.find(options.fileInputSelector),
-            $browseButton = $instance.find($(options.browseButtonSelector));
-
-        // assume the browse button toggles a single file input
-        $browseButton.on('click', function() {
-          $fileInput.trigger('click');
-        });
-
-        // watch for changes to the file input and update the label
-        $fileInput.on('change', function() {
-          $(this).parent().find(options.labelSelector).text($fileInput.val());
-        });
-      }
-
-    });
-
-  })({
-    mainSelector: '.uploader',
-    browseButtonSelector: '[data-action=browse]',
-    fileInputSelector: '[type=file]',
-    labelSelector: '.uploader__label'
-  }, jQuery, window, document);
-
+    };
 });
