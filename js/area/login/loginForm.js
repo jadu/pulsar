c294e97fce76f591f66778610761ae2414b4f78c
Paul Stanton <paul.stanton@jadu.net>
2015-02-27 15:36:31 +0000

    Video login concept

---
+++ js/area/login/loginForm.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var $ = require('jquery'),
+	vide = require('../../../libs/vide/dist/jquery.vide.min');
+
+$(function() {
+	$(".login-backdrop").vide("../../../images/login.mp4");
+});
