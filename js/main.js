8d5d57805b6ca8a7d416785ff8f603f7e7dfa18c
Paul Stanton <paul.stanton@jadu.net>
2015-10-07 21:17:37 +0100

    Tabbing

--- js/main.js
+++ js/main.js
@@ -6,39 +6,39 @@ pulsar.flash           = new pulsar.FlashMessageComponent($html);
 pulsar.signIn          = new pulsar.SignInComponent($html);
 pulsar.masterSwitch    = new pulsar.MasterSwitchComponent($html);
 
-    $(function () {
+$(function () {
 
-        pulsar.buttonComponent.init();
-        pulsar.flash.init();
-        pulsar.signIn.init();
-        pulsar.masterSwitch.init();
+    pulsar.buttonComponent.init();
+    pulsar.flash.init();
+    pulsar.signIn.init();
+    pulsar.masterSwitch.init();
 
-        // Switch out .svg for .png for <img> elements in older browsers
-        pulsar.svgeezy.init('nocheck', 'png');
+    // Switch out .svg for .png for <img> elements in older browsers
+    pulsar.svgeezy.init('nocheck', 'png');
 
-        // Use clickover enhancements for popovers
-        $('[rel="clickover"]').clickover({ 'global_close': true });
+    // Use clickover enhancements for popovers
+    $('[rel="clickover"]').clickover({ 'global_close': true });
 
-        // Select2 elements created by form.select2() helper
-        $('.js-select2').select2();
+    // Select2 elements created by form.select2() helper
+    $('.js-select2').select2();
 
-        $('.mobile-menu-button').on('click', function(e) {
-            e.preventDefault();
+    $('.mobile-menu-button').on('click', function(e) {
+        e.preventDefault();
 
-            $('body').toggleClass('open-nav');
-            $(this).toggleClass('open');
+        $('body').toggleClass('open-nav');
+        $(this).toggleClass('open');
 
-            if ($(this).text() == 'Menu') {
-                $(this).text('Close');
-            } else {
-                $(this).text('Menu');
-            }
-        });
-
-        // Refresh datatables when tabs are switched, this fixes some layout issues
-        $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-            $($.fn.dataTable.tables(true)).DataTable().columns.adjust().responsive.recalc();
-            console.log('!');
-        });
-
+        if ($(this).text() == 'Menu') {
+            $(this).text('Close');
+        } else {
+            $(this).text('Menu');
+        }
     });
+
+    // Refresh datatables when tabs are switched, this fixes some layout issues
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        $($.fn.dataTable.tables(true)).DataTable().columns.adjust().responsive.recalc();
+        console.log('!');
+    });
+
+});
