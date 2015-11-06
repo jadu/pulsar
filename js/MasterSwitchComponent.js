669029f95ebbde51bbc707de59df00ea9c8c073e
Paul Stanton <paul.stanton@jadu.net>
2015-11-06 11:31:30 +0000

    Remove 'masterswitch' var
    Conflicts:
    	js/MasterSwitchComponent.js

--- js/MasterSwitchComponent.js
+++ js/MasterSwitchComponent.js
@@ -1,97 +1,98 @@
 'use strict';
 
 var $ = require('jquery'),
-	toggles = require('../libs/jquery-toggles/toggles.min');
+    toggles = require('../libs/jquery-toggles/toggles.min');
 
 function MasterSwitchComponent(html) {
-	this.$html = html;
+
+    this.$html = html;
+
 };
 
 MasterSwitchComponent.prototype.init = function () {
 
-	var component = this;
+    var component = this;
 
-	component.$container = this.$html.find('.masterswitch');
+    component.$container = this.$html.find('.masterswitch');
 
-	component.$container.each(function() {
+    component.$container.each(function() {
 
-		var $this = $(this),
-			masterswitch;
+        var $this = $(this)
 
-		masterswitch.$control = $this.find('.masterswitch-control input');
-		masterswitch.$content = $this.find('.masterswitch-content');
-		masterswitch.$toggle = $this.find('.toggle');
+        this.$control = $this.find('.masterswitch-control input');
+        this.$content = $this.find('.masterswitch-content');
+        this.$toggle = $this.find('.toggle');
 
-		masterswitch.$toggle.toggles({
-			checkbox: masterswitch.$control,
-			on: masterswitch.$control.prop('checked'),
-			text: {
-				on: '',
-				off: ''
-			}
-		});
+        this.$toggle.toggles({
+            checkbox: this.$control,
+            on: this.$control.prop('checked'),
+            text: {
+                on: '',
+                off: ''
+            }
+        });
 
-		if (!masterswitch.$control.prop('checked')) {
-			component.disableElements(component.$container);
-		} else {
-			component.switchOn(masterswitch.$control);
-		}
+        if (!this.$control.prop('checked')) {
+            component.disableElements(component.$container);
+        } else {
+            component.switchOn(this.$control);
+        }
 
-		masterswitch.$toggle.on('toggle', function (e, active) {
-			if (active) {
-				component.switchOn(e.target);
-			} else {
-				component.switchOff(e.target);
-			}
-		});
-	});
+        this.$toggle.on('toggle', function (e, active) {
+            if (active) {
+                component.switchOn(e.target);
+            } else {
+                component.switchOff(e.target);
+            }
+        });
+    });
 
 };
 
 MasterSwitchComponent.prototype.switchOn = function (target) {
 
-	var component = this;
+    var component = this;
 
-	component.$target = $(target).closest('.masterswitch').find('.masterswitch-content');
+    component.$target = $(target).closest('.masterswitch').find('.masterswitch-content');
 
-	component.$target.removeClass('is-disabled');
-	component.enableElements(component.$target);
+    component.$target.removeClass('is-disabled');
+    component.enableElements(component.$target);
 };
 
 MasterSwitchComponent.prototype.switchOff = function (target) {
 
-	var component = this;
+    var component = this;
 
-	component.$target = $(target).closest('.masterswitch').find('.masterswitch-content');
+    component.$target = $(target).closest('.masterswitch').find('.masterswitch-content');
 
-	component.$target.addClass('is-disabled');
-	component.disableElements(component.$target);
+    component.$target.addClass('is-disabled');
+    component.disableElements(component.$target);
 
 };
 
 MasterSwitchComponent.prototype.disableElements = function (target) {
 
-	var component = this,
-		CLICKABLES_SELECTOR = 'a, button, input, select';
+    var component = this,
+        CLICKABLES_SELECTOR = 'a, button, input, select';
 
-	$(target)
-		.on('click', CLICKABLES_SELECTOR, preventDefault)
-		.find(CLICKABLES_SELECTOR)
-			.addClass('disabled')
-			.attr('disabled', 'disabled');
+    $(target)
+        .on('click', CLICKABLES_SELECTOR, preventDefault)
+        .find(CLICKABLES_SELECTOR)
+            .addClass('disabled')
+            .attr('disabled', 'disabled');
 
 };
 
 MasterSwitchComponent.prototype.enableElements = function (target) {
 
-	var component = this,
-		CLICKABLES_SELECTOR = 'a, button, input, select';
+    var component = this,
+        CLICKABLES_SELECTOR = 'a, button, input, select';
 
-	$(target)
-		.off('click', CLICKABLES_SELECTOR, preventDefault)
-		.find(CLICKABLES_SELECTOR)
-			.removeClass('disabled')
-			.removeAttr('disabled')
+    $(target)
+        .off('click', CLICKABLES_SELECTOR, preventDefault)
+        .find(CLICKABLES_SELECTOR)
+            .removeClass('disabled')
+            .removeAttr('disabled')
 
 };
 
