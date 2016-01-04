93025314f56ce534a2c5c6021bf4f87d98093811
Paul Stanton <paul.stanton@jadu.net>
2016-01-04 10:02:28 +0000

    [Lexicon] Temporarily disable datatables examples until I figure out what is throwing errors

--- js/main.js
+++ js/main.js
@@ -1,77 +1,79 @@
+(function ($) {
 
-var $html = $('html');
+    // Protect IE8 from any erroneous console.log uses which would break everything
+    if (!window.console) {
+        console = { log: function() {} }
+    };
 
-pulsar.button       = new pulsar.ButtonComponent($html);
-pulsar.flash        = new pulsar.FlashMessageComponent($html);
-pulsar.pulsarForm   = new pulsar.PulsarFormComponent($html);
-pulsar.signIn       = new pulsar.SignInComponent($html);
-pulsar.masterSwitch = new pulsar.MasterSwitchComponent($html);
-pulsar.navMain      = new pulsar.NavMainComponent($html);
+    var $html = $('html');
 
-$(function () {
+    pulsar.button       = new pulsar.ButtonComponent($html);
+    pulsar.flash        = new pulsar.FlashMessageComponent($html);
+    pulsar.pulsarForm   = new pulsar.PulsarFormComponent($html);
+    pulsar.signIn       = new pulsar.SignInComponent($html);
+    pulsar.masterSwitch = new pulsar.MasterSwitchComponent($html);
+    pulsar.navMain      = new pulsar.NavMainComponent($html);
 
-    pulsar.button.init();
-    pulsar.flash.init();
-    pulsar.pulsarForm.init();
-    pulsar.signIn.init();
-    pulsar.masterSwitch.init();
-    pulsar.navMain.init();
+    $(function () {
 
-    // Switch out .svg for .png for <img> elements in older browsers
-    pulsar.svgeezy.init('nocheck', 'png');
+        pulsar.button.init();
+        pulsar.flash.init();
+        pulsar.pulsarForm.init();
+        pulsar.signIn.init();
+        pulsar.masterSwitch.init();
+        pulsar.navMain.init();
 
-    // Use clickover enhancements for popovers
-    $('[rel="clickover"]').clickover({ 'global_close': true });
+        // Switch out .svg for .png for <img> elements in older browsers
+        pulsar.svgeezy.init('nocheck', 'png');
 
-    // Open navigation (should be added to NavMainComponent)
-    $('.mobile-menu-button').on('click', function(e) {
-        e.preventDefault();
+        // Use clickover enhancements for popovers
+        $('[rel="clickover"]').clickover({ 'global_close': true });
 
-        $('body').toggleClass('open-nav');
-        $(this).toggleClass('open');
+        // Open navigation (should be added to NavMainComponent)
+        $('.mobile-menu-button').on('click', function(e) {
+            e.preventDefault();
 
-        if ($(this).text() == 'Menu') {
-            $(this).text('Close');
-        } else {
-            $(this).text('Menu');
-        }
+            $('body').toggleClass('open-nav');
+            $(this).toggleClass('open');
+
+            if ($(this).text() == 'Menu') {
+                $(this).text('Close');
+            } else {
+                $(this).text('Menu');
+            }
+        });
+
+        // Refresh datatables when tabs are switched, this fixes some layout issues
+        $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+            $($.fn.dataTable.tables(true)).DataTable().columns.adjust().responsive.recalc();
+            console.log('!');
+        });
+
+    	// var table = $('.datatable').DataTable({
+    	// 	dom: '<"dataTables_top"irf><"dataTables_actions"T>t<"dataTables_bottom"lp>',
+    	// 	aaSorting: [],
+    	// 	bAutoWidth: false,
+    	// 	columnDefs: [
+    	// 		{ "searchable": false, "targets": 0 },
+    	// 		{ "orderable": false, "targets": 0 }
+    	// 	],
+    	// 	oLanguage: {
+     //         sSearch: "Filter:"
+    	//     },
+    	// 	stateSave: false,
+     //        tableTools: {
+     //            sRowSelect: "multi",
+     //            sRowSelector: '.js-select',
+     //            aButtons: [
+     //            	{
+     //                    "sExtends":    "collection",
+     //                    "sButtonText": '<i class="icon-check-minus"></i>',
+     //                    "aButtons":    [ "select_all", "select_none" ]
+     //                }
+     //            ]
+     //        }
+    	// });
+
     });
 
-    // Refresh datatables when tabs are switched, this fixes some layout issues
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-        $($.fn.dataTable.tables(true)).DataTable().columns.adjust().responsive.recalc();
-        console.log('!');
-    });
-
-	var table = $('.datatable').DataTable({
-		dom: '<"dataTables_top"irf><"dataTables_actions"T>t<"dataTables_bottom"lp>',
-		aaSorting: [],
-		bAutoWidth: false,
-		columnDefs: [
-			{ "searchable": false, "targets": 0 },
-			{ "orderable": false, "targets": 0 }
-		],
-		oLanguage: {
-         sSearch: "Filter:"
-	    },
-		responsive: {
-			details: {
-	            type: 'column',
-	            target: '.table-child-toggle'
-	        }
-        },
-		stateSave: false,
-        tableTools: {
-            sRowSelect: "multi",
-            sRowSelector: '.js-select',
-            aButtons: [
-            	{
-                    "sExtends":    "collection",
-                    "sButtonText": '<i class="icon-check-minus"></i>',
-                    "aButtons":    [ "select_all", "select_none" ]
-                }
-            ]
-        }
-	});
-
-});
+}(jQuery));
