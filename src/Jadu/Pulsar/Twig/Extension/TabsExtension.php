1c47ee87650ae6a20847c469e7b3aec5ff49f66e
Paul Stanton <paul.stanton@jadu.net>
2016-02-19 15:06:43 +0000

    Revert removal of return false from TabsExtension

--- src/Jadu/Pulsar/Twig/Extension/TabsExtension.php
+++ src/Jadu/Pulsar/Twig/Extension/TabsExtension.php
@@ -4,54 +4,59 @@ namespace Jadu\Pulsar\Twig\Extension;
 
 class TabsExtension extends \Twig_Extension
 {
-  public function getName()
-  {
-    return 'tabs_extension';
-  }
-
-  public function getFunctions()
-  {
-    return array(
-      new \Twig_SimpleFunction('get_active_tab', array($this, 'getActiveParentTabID'))
-    );
-  }
-
-  public function getActiveParentTabID($tabs = null, $active_parent_tab_id = null)
-  {
-    if ($tabs === null) {
-      return false;
+    public function getName()
+    {
+        return 'tabs_extension';
     }
 
-    // $tabs passed through Twig is an array rather than a json string
-    if (is_array($tabs)) {
-      $tabs = json_encode($tabs);
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('get_active_tab', array($this, 'getActiveParentTabID'))
+            );
     }
 
-    $ob = json_decode($tabs);
-
-    // If we don't have valid json, abandon ship
-    if ($ob === null) {
-      return false;
-    }
-
-    // If no active parent ID has been set, return the first tab ID
-    if ($active_parent_tab_id === null) {
-        return $ob[0]->id;
-    }
-
-    // Loop through the tab items
-    foreach ($ob as $item) {
-
-      // If we have subnavigation
-      if (isset($item->sub_tabs) && $item->sub_tabs != null) {
-
-        // Loop through its sub tabs
-        foreach($item->sub_tabs as $sub_tab) {
-          if ($sub_tab->id === $active_parent_tab_id) {
-            return $item->id;
-          }
+    public function getActiveParentTabID($tabs = null, $active_parent_tab_id = null)
+    {
+        if ($tabs === null) {
+            return false;
         }
-      }
+
+        // $tabs passed through Twig is an array rather than a json string
+        if (is_array($tabs)) {
+            $tabs = json_encode($tabs);
+        }
+
+        $ob = json_decode($tabs);
+
+        // If we don't have valid json, abandon ship
+        if ($ob === null) {
+            return false;
+        }
+
+        // If no active parent ID has been set, return the first tab ID
+        if ($active_parent_tab_id === null) {
+            return $ob[0]->id;
+        }
+
+        // Loop through the tab items
+        foreach ($ob as $item) {
+
+            // If we have subnavigation
+            if (isset($item->sub_tabs) && $item->sub_tabs != null) {
+
+                // Loop through its sub tabs
+                foreach($item->sub_tabs as $sub_tab) {
+                    if ($sub_tab->id === $active_parent_tab_id) {
+                        return $item->id;
+                    }
+                }
+
+            }
+        }
+
+        return false;
+
     }
-  }
+
 }
