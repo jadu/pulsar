27a0e901c369b2ee15dfa2c7730cea6540748079
Dave Irvine <dave@daveirvine.me>
2016-03-04 15:44:54 +0000

    Add JS implementation of ArrayExtension

---
+++ src/Jadu/Pulsar/Twig.js/Extension/ArrayExtension.js
@@ -0,0 +1,71 @@
+var _ = require('lodash');
+
+function ArrayExtension() {
+}
+
+ArrayExtension.prototype.getName = function () {
+	return 'array_extension';
+}
+
+ArrayExtension.prototype.excludeFromArray = function (arr, exclude) {
+	arr = _.fromPairs(_.toPairs(arr));
+
+	if (typeof(arr) !== 'object') {
+		console.log('stuff');
+		return arr;
+	}
+
+	if (exclude === false || exclude === null || exclude === '') {
+		return arr;
+	}
+
+	if (typeof(exclude) === 'string') {
+		exclude = exclude.split(' ');
+	}
+
+	if (exclude.length === 0) {
+		return arr;
+	}
+
+	exclude = _.map(exclude, function (value) {
+		return value.toLowerCase();
+	});
+
+	var omitted = _.filter(_.keys(arr), function (value, key) {
+		return !_.includes(exclude, value);
+	});
+
+	var final = _.pick(arr,omitted);
+
+	return final;
+};
+
+ArrayExtension.prototype.onlyFromArray = function (arr, only) {
+	arr = _.fromPairs(_.toPairs(arr));
+
+	if (typeof(arr) !== 'object') {
+		return arr;
+	}
+
+	if (only === false || only === null || only === '') {
+                return {};
+       	}
+
+        if (typeof(only) === 'string') {
+                only = only.split(' ');
+       	}
+
+        if (only.length === 0) {
+               	return {};
+       	}
+
+        only = _.map(only, function (value) {
+               	return value.toLowerCase();
+       	});
+
+	var included = _.pick(arr, only);
+
+	return included;
+};
+
+module.exports = ArrayExtension;
