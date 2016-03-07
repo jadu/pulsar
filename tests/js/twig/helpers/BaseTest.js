2f48f2ef10fe5544a8f150a54ecad9239a0865f0
Dave Irvine <dave@daveirvine.me>
2016-03-07 15:51:50 +0000

    Whitespace linting

--- tests/js/twig/helpers/BaseTest.js
+++ tests/js/twig/helpers/BaseTest.js
@@ -24,101 +24,101 @@ phpRuntime.install({
 });
 
 function BaseTest() {
-	this.testsPath = path.resolve(__dirname + '/../../../unit/Jadu/Pulsar/Twig/Extension/');
-	this.injectionsPath = path.resolve(__dirname + '/../injections/');
-	this.asserter = new Asserter(this);
+    this.testsPath = path.resolve(__dirname + '/../../../unit/Jadu/Pulsar/Twig/Extension/');
+    this.injectionsPath = path.resolve(__dirname + '/../injections/');
+    this.asserter = new Asserter(this);
 }
 
 BaseTest.prototype.loadPreInjections = function () {
-	var preInjectionsPath = path.resolve(this.injectionsPath + '/pre/');
-	var preInjections = fs.readdirSync(preInjectionsPath);
+    var preInjectionsPath = path.resolve(this.injectionsPath + '/pre/');
+    var preInjections = fs.readdirSync(preInjectionsPath);
 
-	var loadedPreInjections = {};
-	_.each(preInjections, function (injection) {
-		var preInjectionPath = path.resolve(preInjectionsPath + '/' + injection);
-		loadedPreInjections[injection] = fs.readFileSync(preInjectionPath).toString();
-	});
+    var loadedPreInjections = {};
+    _.each(preInjections, function (injection) {
+        var preInjectionPath = path.resolve(preInjectionsPath + '/' + injection);
+        loadedPreInjections[injection] = fs.readFileSync(preInjectionPath).toString();
+    });
 
-	this.preInjections = loadedPreInjections;
+    this.preInjections = loadedPreInjections;
 };
 
 BaseTest.prototype.loadPostInjections = function () {
-	var postInjectionsPath = path.resolve(this.injectionsPath + '/post/');
-	var postInjections = fs.readdirSync(postInjectionsPath);
+    var postInjectionsPath = path.resolve(this.injectionsPath + '/post/');
+    var postInjections = fs.readdirSync(postInjectionsPath);
 
-	var loadedPostInjections = {};
-	_.each(postInjections, function (injection) {
-		var postInjectionPath = path.resolve(postInjectionsPath + '/' + injection);
-		loadedPostInjections[injection] = fs.readFileSync(postInjectionPath).toString();
-	});
+    var loadedPostInjections = {};
+    _.each(postInjections, function (injection) {
+        var postInjectionPath = path.resolve(postInjectionsPath + '/' + injection);
+        loadedPostInjections[injection] = fs.readFileSync(postInjectionPath).toString();
+    });
 
-	this.postInjections = loadedPostInjections;
+    this.postInjections = loadedPostInjections;
 };
 
 BaseTest.prototype.loadInjections = function () {
-	this.loadPreInjections();
-	this.loadPostInjections();
+    this.loadPreInjections();
+    this.loadPostInjections();
 };
 
 BaseTest.prototype.rewriteTest = function (test) {
-	//Strip out the opening php tag
-	test = test.replace(/^[\s]*\<\?php\n/, '');
+    //Strip out the opening php tag
+    test = test.replace(/^[\s]*\<\?php\n/, '');
 
-	var preInjections = '';
-	//Inject all pre injections
-	_.each(this.preInjections, function (preInjection) {
-		preInjections += preInjection;
-	});
+    var preInjections = '';
+    //Inject all pre injections
+    _.each(this.preInjections, function (preInjection) {
+        preInjections += preInjection;
+    });
 
-	test = preInjections + test;
+    test = preInjections + test;
 
-	//Reattach the opening php tag
-	test = '<?php\n' + test;
+    //Reattach the opening php tag
+    test = '<?php\n' + test;
 
-	//Strip the closing brace on the class
-	test = test.replace(/}[\s]*$/, '');
+    //Strip the closing brace on the class
+    test = test.replace(/}[\s]*$/, '');
 
-	//Inject all post injections
-	_.each(this.postInjections, function (postInjection) {
-		test += postInjection;
-	});
+    //Inject all post injections
+    _.each(this.postInjections, function (postInjection) {
+        test += postInjection;
+    });
 
-	//Remove namespaces
-	test = test.replace('namespace Jadu\\Pulsar\\Twig\\Extension;\n', '');
+    //Remove namespaces
+    test = test.replace('namespace Jadu\\Pulsar\\Twig\\Extension;\n', '');
 
-	return test;
+    return test;
 };
 
 BaseTest.prototype.calculateTestsToRun = function (test) {
-	//Find tests to run by parsing the PHP file.
-	//Uniter does not yet support class reflection.
-	var testsToRun = [];
-	var testNameRegex = /public function test([\S]*)\(\)/g;
-	var testNames;
-	while ((testNames = testNameRegex.exec(test)) !== null) {
-		var testName = testNames[1];
-		testsToRun.push('test' + testName);
-	}
+    //Find tests to run by parsing the PHP file.
+    //Uniter does not yet support class reflection.
+    var testsToRun = [];
+    var testNameRegex = /public function test([\S]*)\(\)/g;
+    var testNames;
+    while ((testNames = testNameRegex.exec(test)) !== null) {
+        var testName = testNames[1];
+        testsToRun.push('test' + testName);
+    }
 
-	this.testsToRun = testsToRun;
+    this.testsToRun = testsToRun;
 };
 
 BaseTest.prototype.setTestName = function (testName) {
-	this.nextTest = testName;
+    this.nextTest = testName;
 };
 
 BaseTest.prototype.run = function () {
-	this.test = this.testName + '.php';
+    this.test = this.testName + '.php';
 
-	var testInstance = this;
+    var testInstance = this;
 
-	this.loadInjections();
+    this.loadInjections();
 
-	var testPath = path.resolve(this.testsPath + '/' + this.test);
-	var test = fs.readFileSync(testPath).toString();
+    var testPath = path.resolve(this.testsPath + '/' + this.test);
+    var test = fs.readFileSync(testPath).toString();
 
-	this.calculateTestsToRun(test);
-	test = this.rewriteTest(test);
+    this.calculateTestsToRun(test);
+    test = this.rewriteTest(test);
 
     var wrapper = new Function('return ' + phpToJS.transpile(phpParser.parse(test), {bare: true}) + ';')(),
         module = phpRuntime.compile(wrapper),
@@ -136,9 +136,9 @@ BaseTest.prototype.run = function () {
     engine.expose(this, 'nameSetter');
     engine.expose(this.asserter, 'asserter');
 
-	describe(this.testName, function () {
+    describe(this.testName, function () {
         engine.execute();
-	});
+    });
 };
 
 module.exports = BaseTest;
