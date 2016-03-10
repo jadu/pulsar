6e04b251f36ed829b94a65b4bf50d7d252703710
Dave Irvine <dave@daveirvine.me>
2016-03-10 15:47:01 +0000

    Linting

--- tests/js/twig/helpers/Asserter.js
+++ tests/js/twig/helpers/Asserter.js
@@ -1,45 +1,45 @@
 var Asserter = function (testInstance) {
-	//Store a reference to the test instance so we can access the test name later
-	this.testInstance = testInstance;
+    //Store a reference to the test instance so we can access the test name later
+    this.testInstance = testInstance;
 };
 
 //Provide a generic equality asserter
 Asserter.prototype.equal = function (expected, actual) {
-	//Convert strange Uniter arrays to Objects
-	if (_.isArray(expected)) {
-		expected = _.fromPairs(_.toPairs(expected));
-	}
+    //Convert strange Uniter arrays to Objects
+    if (_.isArray(expected)) {
+        expected = _.fromPairs(_.toPairs(expected));
+    }
 
-	//Unwrap Uniter objects to plain Objects
-	if (actual.objectValue) {
-		actual = actual.objectValue.value;
-	}
+    //Unwrap Uniter objects to plain Objects
+    if (actual.objectValue) {
+        actual = actual.objectValue.value;
+    }
 
-	it(this.testInstance.nextTest, function () {
-		//If the Objects to compare are like Objects with properties, we need to deeply compare them
-		if (_.isObjectLike(expected) && _.isObjectLike(actual)) {
-			return assert.deepEqual(actual, expected);
-		}
+    it(this.testInstance.nextTest, function () {
+        //If the Objects to compare are like Objects with properties, we need to deeply compare them
+        if (_.isObjectLike(expected) && _.isObjectLike(actual)) {
+            return assert.deepEqual(actual, expected);
+        }
 
-		//For all other objects, the normal equality comparator is fine
-		return assert.equal(actual, expected);
-	});
+        //For all other objects, the normal equality comparator is fine
+        return assert.equal(actual, expected);
+    });
 };
 
 Asserter.prototype.contains = function (needle, haystack) {
-	//Convert strange Uniter arrays to Objects
-        if (_.isArray(needle)) {
-                needle = _.fromPairs(_.toPairs(needle));
-        }
+    //Convert strange Uniter arrays to Objects
+    if (_.isArray(needle)) {
+        needle = _.fromPairs(_.toPairs(needle));
+    }
 
-        //Unwrap Uniter objects to plain Objects
-        if (haystack.objectValue) {
-                haystack = haystack.objectValue.value;
-        }
+    //Unwrap Uniter objects to plain Objects
+    if (haystack.objectValue) {
+        haystack = haystack.objectValue.value;
+    }
 
-	it(this.testInstance.nextTest, function () {
-		return assert.include(haystack, needle);
-	});
+    it(this.testInstance.nextTest, function () {
+        return assert.include(haystack, needle);
+    });
 };
 
 module.exports = Asserter;
