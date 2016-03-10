74cf911209eacafc3d7fa44a48b9cffaf424b1d7
Dave Irvine <dave@daveirvine.me>
2016-03-10 12:34:03 +0000

    Fix RelativeTimeExtension to match PHP output

--- src/Jadu/Pulsar/Twig.js/Extension/RelativeTimeExtension.js
+++ src/Jadu/Pulsar/Twig.js/Extension/RelativeTimeExtension.js
@@ -2,6 +2,26 @@ var _ = require('lodash'),
     moment = require('moment');
 
 function RelativeTimeExtension() {
+    moment.locale('en', {
+        relativeTime : {
+            future: "in %s",
+            past:   "%s ago",
+            s:  function (number, withoutSuffix, key, isFuture) {
+                var plural = (number < 2) ? " second" : " seconds";
+                return number + plural;
+            },
+            m:  "%d minute",
+            mm: "%d minutes",
+            h:  "%d hour",
+            hh: "%d hours",
+            d:  "%d day",
+            dd: "%d days",
+            M:  "%d month",
+            MM: "%d months",
+            y:  "%d year",
+            yy: "%d years"
+        }
+    });
 }
 
 RelativeTimeExtension.prototype.getName = function () {
@@ -9,8 +29,18 @@ RelativeTimeExtension.prototype.getName = function () {
 };
 
 RelativeTimeExtension.prototype.timeAgo = function (time_from) {
-    if (!time_from) {
+    if (time_from === undefined || time_from === null) {
         return false;
+    }
+
+    if (_.isDate(time_from)) {
+        time_from = time_from.getTime()/1000;
+    }
+
+    var time_now = new Date().getTime();
+
+    if ((time_from*1000-time_now) === 0) {
+        return 'just now';
     }
 
     return moment.unix(time_from).fromNow();
