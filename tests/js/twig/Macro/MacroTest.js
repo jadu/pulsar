d9101ebed0084f0215054a1730045e3dcfe65ff1
Dave Irvine <dave@daveirvine.me>
2016-04-18 13:50:21 +0100

    Add Macro tests

---
+++ tests/js/twig/Macro/MacroTest.js
@@ -0,0 +1,59 @@
+var _ = require('lodash'),
+    chai = require('chai'),
+    expect = require('chai').expect,
+    fs = require('fs'),
+    path = require('path'),
+    Twig = require('twigged'), // Twig module
+    twig = Twig.twig,       // Compile function
+    Pulsar = require('../../../../')(Twig);
+
+chai.use(require('chai-hiff'));
+
+var fixturesPath = path.resolve(__dirname, '../../../unit/Jadu/Pulsar/Twig/Macro/Fixtures');
+Pulsar.installExtensions();
+
+function normalizeOutput(output) {
+    output = output.split(/\s+/g);
+    output = output.join(' ');
+    output = output.replace(/>\s+/g, '>');
+    output = output.replace(/\s+</g, '<');
+
+    return output;
+}
+
+describe('Macros', function () {
+    var fixtureTypes = fs.readdirSync(fixturesPath);
+
+    _.each(fixtureTypes, function (fixtureType) {
+        var fixturePath = path.resolve(fixturesPath, fixtureType);
+        describe(fixtureType, function () {
+            var fixtures = fs.readdirSync(fixturePath);
+            _.each(fixtures, function (fixture) {
+                if (fixture.indexOf('twig') >= 0) {
+                    var base = path.basename(fixture, '.html.twig'),
+                        htmlBase = path.basename(fixture, '.twig');
+
+                    it('should correctly render ' + base + ' template', function () {
+                        var htmlFixturePath = path.resolve(fixturePath, htmlBase);
+                        var htmlFixture = fs.readFileSync(htmlFixturePath).toString();
+                        var twigFixturePath = path.resolve(fixturePath, fixture);
+                        var twigFixture = fs.readFileSync(twigFixturePath).toString();
+
+                        var template = twig({
+                            async: false,
+                            data: twigFixture,
+                            namespaces: {
+                                'pulsar': './views/'
+                            }
+                        });
+
+                        var renderedTemplate = normalizeOutput(template.render() || "");
+                        var expectedValue = normalizeOutput(htmlFixture);
+
+                        expect(renderedTemplate).to.hiffEqual(expectedValue);
+                    });
+                }
+            });
+        });
+    });
+});
