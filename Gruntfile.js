a1c6fd07dfa7a7f3006fdeab76660ca0bdfac796
Paul Stanton <paul.stanton@jadu.net>
2016-11-03 20:56:45 +0000

    Remove IE8 debug code from greeter

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,7 +62,7 @@ module.exports = function(grunt) {
         sass: {
             dev: {
                 options: {
-                    style: 'nested',
+                    outputStyle: 'nested',
                     sourceMap: true
                 },
                 files: [{
