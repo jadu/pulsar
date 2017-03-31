d094ad7ac6fb26d9c4adb93bd23ef303c35edcf0
Nikos Vasileiadis <vasileiadis.nikos@gmail.com>
2017-03-31 12:56:29 +0100

    Add CasperJS Browsing Steps

--- js/casper.js
+++ js/casper.js
@@ -1,7 +1,27 @@
 var fs = require('fs');
+var links;
 
-casper.start('http://google.com', function() {
-    fs.write('dist/htmloutput/page.html', this.getPageContent(), 'w');
+function getLinks() {
+// Scrape the links from primary nav of the website
+    var links = document.querySelectorAll('.nav-link.t-nav-link');
+    return Array.prototype.map.call(links, function (e) {
+        return e.getAttribute('href')
+    });
+}
+
+casper.start('http://192.168.13.37/index.php', function() {
+    fs.write('tests/validation/html_output/index.html', this.getPageContent(), 'w');
+});
+
+casper.then(function () {
+    links = this.evaluate(getLinks);
+    for(var i in links) {
+        console.log(i,' Outer:', links[i]);
+        casper.start(links[i], function() {
+            console.log(i, ' Inner:', links[i]);
+            fs.write('tests/validation/html_output/page-' + i + '.html', this.getPageContent(), 'w');
+        });
+    }
 });
 
 casper.run(function() {
