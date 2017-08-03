917f8bd5c15c7361fd4bae7ee4848fd23f7ab881
Nikos Vasileiadis <vasileiadis.nikos@gmail.com>
2017-08-03 14:58:12 +0100

    Fix Equal signs based on Hound Recommendation

--- js/PulsarSortableComponent.js
+++ js/PulsarSortableComponent.js
@@ -184,10 +184,10 @@ PulsarSortableComponent.prototype.actionsBadge = function() {
         component.$html.find('.has-badges .btn__group.dropdown span.badge').remove();
         $('<span class="badge">'+ checkedBoxesCount +'</span>').insertBefore('.has-badges .btn__group.dropdown span.caret');
 
-        if ($('.has-badges .dropdown__menu li:last-child').text().indexOf("Delete") == 1) {
+        if ($('.has-badges .dropdown__menu li:last-child').text().indexOf("Delete") === 1) {
             $('<span class="badge">'+ checkedBoxesCount +'</span>').appendTo('.has-badges .dropdown__menu li:last-child a');
 
-            if (checkedBoxesCount == 1 ) {
+            if (checkedBoxesCount === 1 ) {
                 component.$html.find('.has-badges .btn__group.dropdown .item-wording').remove();
                 $('<span class="item-wording">item</span>').appendTo('.has-badges .dropdown__menu li:last-child a');
             } else {
