698c5240c220329ad5c6d5f84d4c63d03edc545e
Paul Stanton <paul.stanton@jadu.net>
2017-10-26 14:07:11 +0100

    Hound fixes

--- docs/assets/main.js
+++ docs/assets/main.js
@@ -3,10 +3,9 @@
     // Protect IE8 from any erroneous console.log uses which would break everything
     if (!window.console) {
         console = { log: function() {} }
-    };
+    }
 
-    var $html = $('html'),
-        lt10 = $html.hasClass('lt-ie10');
+    var $html = $('html');
 
     $html.removeClass('no-js');
 
