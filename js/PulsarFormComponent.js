15f417ea19347820977cc4f26cf72a02c6721d98
Nikos Vasileiadis <vasileiadis.nikos@gmail.com>
2017-11-21 11:05:15 +0000

    Update Password Component Criteria

--- js/PulsarFormComponent.js
+++ js/PulsarFormComponent.js
@@ -186,15 +186,16 @@ PulsarFormComponent.prototype.passwordStrengthMeter = function(target) {
         badPass: 'Weak: try combining letters & numbers',
         goodPass: 'Medium: try using special charecters',
         strongPass: 'Strong password',
-        containsUsername: 'The password contains the username',
         enterPass: 'Type your password',
+        showCriteria: true,
+        criteriaPosition: 'up', //up or down
         showPercent: false,
         showText: true, // shows the text tips
         animate: true, // whether or not to animate the progress bar on input blur/focus
         animateSpeed: 'fast', // the above animation speed
         username: false, // select the username field (selector or jQuery instance) for better password checks
-        usernamePartialMatch: true, // whether to check for username partials
-        minimumLength: 12 // minimum password length (below this threshold, the score is 0)
+        usernamePartialMatch: false, // whether to check for username partials
+        minimumLength: 8 // minimum password length (below this threshold, the score is 0)
     });
 }
 
