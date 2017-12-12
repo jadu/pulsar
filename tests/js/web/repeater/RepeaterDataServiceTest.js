035f051a39fbe0f2f6f37773d91f0c38f463cdf5
Mike Smart <mike.smart@jadu.co.uk>
2017-12-12 11:57:54 +0000

    RepeaterDataService tests

--- tests/js/web/repeater/RepeaterDataServiceTest.js
+++ tests/js/web/repeater/RepeaterDataServiceTest.js
@@ -2,7 +2,7 @@ const RepeaterDataService = require('../../../../js/Repeater/RepeaterDataService
 const QueryService = require('../../../../js/utilities/QueryService');
 const InputCloneService = require('../../../../js/Repeater/InputCloneService');
 const InputValueService = require('../../../../js/Repeater/InputValueService');
-const UniqueIdService = require('../../../../js/utilitlies/UniqueIdService');
+const UniqueIdService = require('../../../../js/utilities/UniqueIdService');
 
 describe('RepeaterDataService', () => {
     let repeaterDataService;
@@ -22,11 +22,90 @@ describe('RepeaterDataService', () => {
             inputValueServiceStub,
             uniqueIdServiceStub
         );
+
+        queryServiceStub.getQuery.withArgs('name').returns('[data-name]');
+        queryServiceStub.getAttr.withArgs('name').returns('data-name');
+        queryServiceStub.getAttr.withArgs('saved-entry-id').returns('data-saved-entry-id');
     });
 
     describe('create', () => {
-        it('should create a saved data repeater group from a form group', () => {
+        let $html;
+        let group;
+        let data;
 
+        beforeEach(() => {
+            $html = $(`
+                <div id="root">
+                    <div id="data"></div>
+                    <form id="form">
+                        <input id="test_input" data-name="test_input" type="text" value="foo"/>
+                    </form>
+                </div>
+            `);
+
+            group = $html.find('#form')[0];
+            data = $html.find('#data')[0];
+        });
+
+        it('should create a saved data repeater group from a form group', () => {
+            const clonedInput = group.querySelector('#test_input').cloneNode();
+
+            queryServiceStub.get.withArgs('saved-entries-root').returns(data);
+            inputCloneServiceStub.clone.returns(clonedInput);
+            repeaterDataService.create(group, 666);
+
+            expect(data.children).to.have.length.of(1);
+            expect(data.firstElementChild.getAttribute('data-saved-entry-id')).to.equal('666');
+            expect(clonedInput.getAttribute('name')).to.equal('test_input');
+            expect(clonedInput.getAttribute('data-name')).to.be.null;
+            expect(uniqueIdServiceStub.uniquifyIds).to.have.been.calledOnce;
+        });
+    });
+
+    describe('update', () => {
+        let $html;
+        let data;
+
+        beforeEach(() => {
+            $html = $(`
+                <div id="root">
+                    <div id="data">
+                        <div data-saved-entry-id="666">
+                            <input name="foo" value="foo"/>
+                            <input name="bar" value="bar"/>
+                        </div>
+                    </div>
+                </div>
+            `);
+
+            data = $html.find('#data')[0];
+        });
+
+        it('should invoke the input value service for each matched input in the state', () => {
+            const state = {
+                foo: {
+                    value: [ { value: 'foo', selected: true } ]
+                },
+                bar: {
+                    value: [ { value: 'bar', selected: false } ]
+                }
+            };
+
+            queryServiceStub.get.withArgs('saved-entries-root').returns(data);
+
+            repeaterDataService.update(state, 666);
+
+            expect(inputValueServiceStub.setValue).to.have.been.calledWith(
+                data.querySelector('[name="foo"]'),
+                'foo',
+                { selected: true }
+            );
+
+            expect(inputValueServiceStub.setValue).to.have.been.calledWith(
+                data.querySelector('[name="bar"]'),
+                'bar',
+                { selected: false }
+            );
         });
     });
 });
