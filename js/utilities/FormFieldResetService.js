ce8013e1f9ed48870e249b3c8f425843629ae6e8
Mike Smart <mike.smart@jadu.co.uk>
2018-01-03 09:25:25 +0000

    trigger change event for select2 inputs in the reset service

--- js/utilities/FormFieldResetService.js
+++ js/utilities/FormFieldResetService.js
@@ -9,11 +9,19 @@ class FormFieldResetService {
      */
     reset (root) {
         const $root = $(root);
+        const $select2 = $root.find('.js-select2');
         const $tempFormWrapper = $root
             .wrap('<form></form>').closest('form');
 
         $tempFormWrapper.trigger('reset');
         $root.unwrap($tempFormWrapper);
+
+        // Trigger a change for select2 enabled inputs
+        if ($select2.length) {
+            $select2.each((index, element) => {
+                $(element).change();
+            });
+        }
     }
 }
 
