bd6db58d1a4a757139c4886830e944fe696cfbec
Nikos Vasileiadis <vasileiadis.nikos@gmail.com>
2018-01-29 12:19:12 +0000

    Tidy up Tests

--- tests/js/web/PasswordStrengthCheckerComponentTest.js
+++ tests/js/web/PasswordStrengthCheckerComponentTest.js
@@ -11,51 +11,51 @@ describe('Pulsar Password Library', function() {
         this.$html = $('<div id="html"></div>').appendTo('html');
         this.$body = $('<div id="body"></div>').appendTo(this.$html);
         this.$markup = $('\
-    <form class="form">\
-        <div class="form__group password__meter password__toggle">\
-            <label for="password__metertoggle" class="control__label">Composite Password field with Meter and Toggle</label>\
-            <div class="controls">\
-                <div class="input-group has-btn-appended">\
-                    <input id="password__metertoggle" type="password" class="form__control">\
-                        <div class="password__wrapper form__control" style="display: none;">\
-                            <div class="password__graybar">\
-                                <div class="password__colorbar" style="background-position: 0px 0px; width: 0%;">\
+            <form class="form">\
+                <div class="form__group password__meter password__toggle">\
+                    <label for="password__metertoggle" class="control__label">Composite Password field with Meter and Toggle</label>\
+                    <div class="controls">\
+                        <div class="input-group has-btn-appended">\
+                            <input id="password__metertoggle" type="password" class="form__control">\
+                                <div class="password__wrapper form__control" style="display: none;">\
+                                    <div class="password__graybar">\
+                                        <div class="password__colorbar" style="background-position: 0px 0px; width: 0%;">\
+                                        </div>\
+                                    </div>\
+                                    <span class="password__text">Type your password</span>\
                                 </div>\
+                            <span class="input-group-btn"><button id="password__metertoggle__button" type="button" class="btn btn--naked"><i aria-hidden="true" class="icon-eye"></i></button></span>\
+                        </div>\
+                    </div>\
+                </div>\
+                <div class="form__group password__meter password__toggle">\
+                <label for="password__metertoggle-2" class="control__label">Composite Password field with Meter and Toggle (Text)</label>\
+                    <div class="controls">\
+                        <div class="input-group has-btn-appended">\
+                            <input id="password__metertoggle-2" type="password" class="form__control" style="border-bottom-left-radius: 4px; border-bottom-right-radius: 4px;">\
+                            <div class="password__wrapper form__control" style="display: none;">\
+                                <div class="password__graybar">\
+                                    <div class="password__colorbar" style="background-position: 0px 0px; width: 0%;"></div>\
+                                </div>\
+                                <span class="password__percent">0%</span>\
+                                <span class="password__text">Type your password</span>\
                             </div>\
-                            <span class="password__text">Type your password</span>\
+                            <div class="password__criteria">\
+                                <ul>\
+                                    <li class="minimumLength"><span>8 characters minimum</span></li>\
+                                    <li class="uppercase"><span>1 UPPERCASE character</span></li>\
+                                    <li class="lowercase"><span>1 lowercase character</span></li>\
+                                    <li class="specialChar"><span>1 Special character</span></li>\
+                                    <li class="number"><span>1 number</span></li>\
+                                    <li class="commonPassword"><span>Not a common password</span></li>\
+                                </ul>\
+                            </div>\
+                            <span class="input-group-btn"><button id="password__metertoggle__btn-2" type="button" class="btn password__texttoggle__btn btn--naked">SHOW</button></span>\
                         </div>\
-                    <span class="input-group-btn"><button id="password__metertoggle__button" type="button" class="btn btn--naked"><i aria-hidden="true" class="icon-eye"></i></button></span>\
-                </div>\
-            </div>\
-        </div>\
-        <div class="form__group password__meter password__toggle">\
-        <label for="password__metertoggle-2" class="control__label">Composite Password field with Meter and Toggle (Text)</label>\
-            <div class="controls">\
-                <div class="input-group has-btn-appended">\
-                    <input id="password__metertoggle-2" type="password" class="form__control" style="border-bottom-left-radius: 4px; border-bottom-right-radius: 4px;">\
-                    <div class="password__wrapper form__control" style="display: none;">\
-                        <div class="password__graybar">\
-                            <div class="password__colorbar" style="background-position: 0px 0px; width: 0%;"></div>\
-                        </div>\
-                        <span class="password__percent">0%</span>\
-                        <span class="password__text">Type your password</span>\
                     </div>\
-                    <div class="password__criteria">\
-                        <ul>\
-                            <li class="minimumLength"><span>8 characters minimum</span></li>\
-                            <li class="uppercase"><span>1 UPPERCASE character</span></li>\
-                            <li class="lowercase"><span>1 lowercase character</span></li>\
-                            <li class="specialChar"><span>1 Special character</span></li>\
-                            <li class="number"><span>1 number</span></li>\
-                            <li class="commonPassword"><span>Not a common password</span></li>\
-                        </ul>\
-                    </div>\
-                    <span class="input-group-btn"><button id="password__metertoggle__btn-2" type="button" class="btn password__texttoggle__btn btn--naked">SHOW</button></span>\
                 </div>\
-            </div>\
-        </div>\
-    </form>\
-').appendTo(this.$body);
+            </form>\
+        ').appendTo(this.$body);
 
         this.$password = this.$html.find('#password__metertoggle');
         this.$eyeButton = this.$html.find('#password__metertoggle__button');
