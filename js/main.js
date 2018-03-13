12ad9677f5536c64a63fc504afb6f3265a43f801
Paul Bele <paul.bele@jadu.net>
2018-03-13 17:22:46 +0200

    Fix tab indentation in main.js

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@
     pulsar.pulsarSortable = new pulsar.PulsarSortableComponent($html, window);
     pulsar.signIn = new pulsar.SignInComponent($html);
     pulsar.masterSwitch = new pulsar.MasterSwitchComponent($html, pulsar.disableUi);
-	pulsar.modulePermissions = new pulsar.ModulePermissionsComponent($html);
+    pulsar.modulePermissions = new pulsar.ModulePermissionsComponent($html);
     pulsar.navMain = new pulsar.NavMainComponent($html, window);
     pulsar.filterBar = new pulsar.FilterBarComponent($html);
     pulsar.faviconEditor = new pulsar.FaviconEditor(document.head);
