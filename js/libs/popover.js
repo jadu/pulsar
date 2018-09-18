b4dca64a42a1b91e3bb1d19e269c8e9b28cc895a
Paul Stanton <paul@coffeepowered.co.uk>
2018-09-18 10:19:00 +0100

    Stop guidance popovers requiring clickover dependency
    Also fixes issue where clicking on the guidance icon would trigger the focus (and selection of) checkboxes and radios

--- js/libs/popover.js
+++ js/libs/popover.js
@@ -115,7 +115,21 @@ var $ = require('jquery'),
   }
 
   $(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
+    $('[data-toggle="popover"]')
+    .on('click', function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+    }).popover();
+  });
+
+  $(document).on('click', function (e) {
+    $('[data-toggle="popover"][data-autoclose="true"]').each(function () {
+        //the 'is' for buttons that trigger popups
+        //the 'has' for icons within a button that triggers a popup
+        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {                
+            (($(this).popover('hide').data('bs.popover')||{}).inState||{}).click = false  // fix for BS 3.3.6
+        }
+    });
   });
 
 module.exports = Popover;
