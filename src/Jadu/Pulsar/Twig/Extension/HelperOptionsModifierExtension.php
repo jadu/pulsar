4658d2fd8319f8fa7b53ae7e72d8bc76bd085e49
Nikos Vasileiadis <vasileiadis.nikos@gmail.com>
2018-09-18 14:39:50 +0100

    Improve Syntax

--- src/Jadu/Pulsar/Twig/Extension/HelperOptionsModifierExtension.php
+++ src/Jadu/Pulsar/Twig/Extension/HelperOptionsModifierExtension.php
@@ -41,10 +41,9 @@ class HelperOptionsModifierExtension extends \Twig_Extension
 
         // Check if errors are present in options
         if (array_key_exists('error', $options) && !empty($options['error'])) {
-            if (!is_array($options['error'])) {
-                $options['error'] = [$options['error']];
-            }
-            $numberOfErrors = count($options['error']);
+            $errors = is_array($options['error']) ?  $options['error'] : [$options['error']];
+
+            $numberOfErrors = count($errors);
             $errorGuids = array();
 
             // Set has_error for aria-invalid use
