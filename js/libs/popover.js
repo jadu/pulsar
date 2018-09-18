4fb8202934dbda713397640cc2e012d229de8d06
Paul Stanton <paul.stanton@jadu.co.uk>
2018-09-18 11:06:35 +0100

    Simpler JS as per Mike’s guidance

--- js/libs/popover.js
+++ js/libs/popover.js
@@ -124,11 +124,12 @@ var $ = require('jquery'),
 
   $(document).on('click', function (e) {
     $('[data-toggle="popover"][data-autoclose="true"], [rel="clickover"]').each(function () {
-        //the 'is' for buttons that trigger popups
-        //the 'has' for icons within a button that triggers a popup
-        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {                
-            (($(this).popover('hide').data('bs.popover')||{}).inState||{}).click = false  // fix for BS 3.3.6
-        }
+      var $this = $(this),
+          $bsPopover = $this.popover('hide').data('bs.popover');
+
+      if ($bsPopover !== undefined && $bsPopover.inState) {
+        $bsPopover.click = false;
+      }
     });
   });
 
