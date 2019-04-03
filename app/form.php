ae58da43e96ff821a9c07f85a02827a25527a7ad
Paul Stanton <paul@coffeepowered.co.uk>
2019-04-03 14:34:17 +0100

    Initial experimentation with implementing a Symfony form with Pulsar styling

---
+++ app/form.php
@@ -0,0 +1,182 @@
+<?php
+
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\CurrencyType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\LanguageType;
+use Symfony\Component\Form\Extension\Core\Type\LocaleType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\PercentType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\Extension\Core\Type\TimezoneType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/forms.php';
+
+$form = $formFactory->createBuilder()
+    ->add('Textfield', TextType::class, array(
+        'label' => 'Text field'
+    ))
+    ->add('Textarea', TextareaType::class, array(
+        'label' => 'Textarea'
+    ))
+    ->add('Email', EmailType::class, array(
+        'label' => 'Email'
+    ))
+    ->add('Integer', IntegerType::class, array(
+        'label' => 'Integer'
+    ))
+    ->add('Money', MoneyType::class, array(
+        'label' => 'Money'
+    ))
+    ->add('Number', NumberType::class, array(
+        'label' => 'Number'
+    ))
+    ->add('Percent', PercentType::class, array(
+        'label' => 'Percent'
+    ))
+    ->add('Search', SearchType::class, array(
+        'label' => 'Search'
+    ))
+    ->add('Url', UrlType::class, array(
+        'label' => 'Url'
+    ))
+    ->add('Range', RangeType::class, array(
+        'label' => 'Range'
+    ))
+    ->add('Tel', TelType::class, array(
+        'label' => 'Tel'
+    ))
+    ->add('Color', ColorType::class, array(
+        'label' => 'Color'
+    ))
+    ->add('ChoiceDefault', ChoiceType::class, array(
+        'label' => 'Choice (default)',
+        'choices'  => [
+            'Maybe' => null,
+            'Yes' => true,
+            'No' => false,
+        ]   
+    ))
+    ->add('ChoiceExpanded', ChoiceType::class, array(
+        'label' => 'Choice (expanded)',
+        'expanded' => true,
+        'choices'  => [
+            'Maybe' => null,
+            'Yes' => true,
+            'No' => false,
+        ]   
+    ))
+    ->add('ChoiceMultiple', ChoiceType::class, array(
+        'label' => 'Choice (multiple)',
+        'multiple' => true,
+        'choices'  => [
+            'Maybe' => null,
+            'Yes' => true,
+            'No' => false,
+        ]   
+    ))
+    ->add('ChoiceExpandedMultiple', ChoiceType::class, array(
+        'label' => 'Choice (expanded multiple)',
+        'expanded' => true,
+        'multiple' => true,
+        'choices'  => [
+            'Maybe' => null,
+            'Yes' => true,
+            'No' => false,
+        ]   
+    ))
+    ->add('Country', CountryType::class, array(
+        'label' => 'Country'
+    ))
+    ->add('Language', LanguageType::class, array(
+        'label' => 'Language'
+    ))
+    ->add('Locale', LocaleType::class, array(
+        'label' => 'Locale'
+    ))
+    ->add('Timezone', TimezoneType::class, array(
+        'label' => 'Timezone'
+    ))
+    ->add('Currency', CurrencyType::class, array(
+        'label' => 'Currency'
+    ))
+    ->add('Date', DateType::class, array(
+        'label' => 'Date'
+    ))
+    ->add('Date', DateType::class, array(
+        'label' => 'Date'
+    ))
+    ->add('DateInterval', DateIntervalType::class, array(
+        'label' => 'Date Interval'
+    ))
+    ->add('DateTime', DateTimeType::class, array(
+        'label' => 'Date Time'
+    ))
+    ->add('Time', TimeType::class, array(
+        'label' => 'Time'
+    ))
+    ->add('Birthday', BirthdayType::class, array(
+        'label' => 'Birthday'
+    ))
+    ->add('Checkbox', CheckboxType::class, array(
+        'label' => 'Checkbox'
+    ))
+    ->add('File', FileType::class, array(
+        'label' => 'File'
+    ))
+    ->add('Radio', RadioType::class, array(
+        'label' => 'Radio'
+    ))
+    ->add('Button', ButtonType::class, array(
+        'label' => 'Button'
+    ))
+    ->add('Reset', ResetType::class, array(
+        'label' => 'Reset'
+    ))
+    ->add('Submit', SubmitType::class, array(
+        'label' => 'Submit'
+    ))
+
+    
+    
+    ->getForm();
+    
+if (isset($_POST[$form->getName()])) {
+    $form->submit($_POST[$form->getName()]);
+
+    if ($form->isValid()) {
+        var_dump('VALID', $form->getData());
+        die;
+    }
+}
+
+$template = $twig->loadTemplate('symfony/index.html.twig');
+
+print $template->render(array(
+    'symfonyForm' => $form->createView(),
+));
---
+++ src/forms.php
@@ -0,0 +1,88 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Jadu\Pulsar\Twig\Extension\ArrayExtension;
+use Jadu\Pulsar\Twig\Extension\AttributeParserExtension;
+use Jadu\Pulsar\Twig\Extension\ConfigExtension;
+use Jadu\Pulsar\Twig\Extension\ConstantDefinedExtension;
+use Jadu\Pulsar\Twig\Extension\GetConstantExtension;
+use Jadu\Pulsar\Twig\Extension\HelperOptionsModifierExtension;
+use Jadu\Pulsar\Twig\Extension\RelativeTimeExtension;
+use Jadu\Pulsar\Twig\Extension\UrlParamsExtension;
+use Jadu\Pulsar\Twig\Extension\TabsExtension;
+
+use Symfony\Component\Validator\Validation;
+use Symfony\Bridge\Twig\Form\TwigRendererEngine;
+use Symfony\Component\Form\Forms;
+use Symfony\Component\Form\FormRenderer;
+use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
+use Symfony\Component\Form\Extension\Csrf\CsrfExtension;
+use Symfony\Component\Security\Csrf\CsrfTokenManager;
+use Symfony\Component\Translation\Translator;
+use Symfony\Component\Translation\Loader\XliffFileLoader;
+use Symfony\Bridge\Twig\Extension\TranslationExtension;
+use Symfony\Bridge\Twig\Extension\FormExtension;
+use Symfony\Bridge\Twig\Form\TwigRenderer;
+
+define('DEFAULT_FORM_THEME', 'form_div_layout.html.twig');
+
+define('VENDOR_DIR', realpath(__DIR__ . '/../vendor'));
+define('VENDOR_FORM_DIR', VENDOR_DIR . '/symfony/form');
+define('VENDOR_VALIDATOR_DIR', VENDOR_DIR . '/symfony/validator');
+define('VENDOR_TWIG_BRIDGE_DIR', VENDOR_DIR . '/symfony/twig-bridge');
+define('VIEWS_DIR', realpath(__DIR__ . '/../views'));
+
+// Set up the CSRF Token Manager
+$csrfTokenManager = new CsrfTokenManager();
+
+// Set up the Validator component
+$validator = Validation::createValidator();
+
+// Set up the Translation component
+$translator = new Translator('en');
+$translator->addLoader('xlf', new XliffFileLoader());
+$translator->addResource('xlf', VENDOR_FORM_DIR . '/Resources/translations/validators.en.xlf', 'en', 'validators');
+$translator->addResource('xlf', VENDOR_VALIDATOR_DIR . '/Resources/translations/validators.en.xlf', 'en', 'validators');
+
+$loader = new Twig_Loader_Filesystem(array(
+    VIEWS_DIR,
+    VENDOR_TWIG_BRIDGE_DIR . '/Resources/views/Form',
+));
+
+$loader->addPath(VIEWS_DIR, 'pulsar');
+
+$twig = new Twig_Environment($loader,
+    array(
+        'debug' => true,
+        'strict_variables' => true
+    )
+);
+
+$formEngine = new TwigRendererEngine(array(DEFAULT_FORM_THEME));
+$formEngine->setEnvironment($twig);
+
+$twig->addExtension(new ArrayExtension());
+$twig->addExtension(new AttributeParserExtension());
+$twig->addExtension(new ConfigExtension(__DIR__ . '/pulsar.json'));
+$twig->addExtension(new ConstantDefinedExtension());
+$twig->addExtension(new HelperOptionsModifierExtension());
+$twig->addExtension(new GetConstantExtension());
+$twig->addExtension(new RelativeTimeExtension());
+$twig->addExtension(new UrlParamsExtension($_GET));
+$twig->addExtension(new TabsExtension());
+$twig->addExtension(new Twig_Extension_Debug());
+$twig->addExtension(new TranslationExtension($translator));
+$twig->addExtension(new FormExtension(new TwigRenderer($formEngine, $csrfTokenManager)));
+
+$twig->addRuntimeLoader(new \Twig_FactoryRuntimeLoader(array(
+    \Symfony\Component\Form\FormRenderer::class => function () use ($formEngine, $csrfManager) {
+        return new \Symfony\Component\Form\FormRenderer($formEngine, $csrfManager);
+    },
+)));
+
+// Set up the Form component
+$formFactory = Forms::createFormFactoryBuilder()
+    ->addExtension(new CsrfExtension($csrfTokenManager))
+    ->addExtension(new ValidatorExtension($validator))
+    ->getFormFactory();
