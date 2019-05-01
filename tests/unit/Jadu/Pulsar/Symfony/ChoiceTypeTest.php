df406cfca0e172895315755f33f5c9456c9a9616
Paul Stanton <paul@coffeepowered.co.uk>
2019-05-01 10:48:21 +0100

    Choice tests

--- tests/unit/Jadu/Pulsar/Symfony/ChoiceTypeTest.php
+++ tests/unit/Jadu/Pulsar/Symfony/ChoiceTypeTest.php
@@ -75,131 +75,79 @@ class ChoiceTypeTest extends Symfony_TestCase
             
         $this->compareOutput($form, 'choice-help.html.twig');
     }
-    
-    // /**
-    //  * @group choice
-    //  */
-    // public function testChoiceFieldHelp ()
-    // {
-    //     $form = $this->formFactory->createBuilder()
-    //         ->add('field', ChoiceType::class, array(
-    //             'label' => 'foo',
-    //             'required' => false,
-    //             'attr' => [
-    //                 'data-help-text' => 'my help text',
-    //             ]
-    //         ))
-    //         ->getForm();
-            
-    //     $this->compareOutput($form, 'choice-help.html.twig');
-    // }
 
-    // /**
-    //  * @group choice
-    //  */
-    // public function testChoiceFieldGuidanceText ()
-    // {
-    //     $form = $this->formFactory->createBuilder()
-    //         ->add('field', ChoiceType::class, array(
-    //             'label' => 'foo',
-    //             'required' => false,
-    //             'attr' => [
-    //                 'data-guidance-text' => 'foo <span class="bar">bar</span>'
-    //             ]
-    //         ))
-    //         ->getForm();
+    /**
+     * @group choice
+     */
+    public function testChoiceFieldRadiosGuidanceText ()
+    {
+        $form = $this->formFactory->createBuilder()
+            ->add('field', ChoiceType::class, array(
+                'label' => 'foo',
+                'required' => false,
+                'expanded' => true,
+                'multiple' => false,
+                'choices'  => [
+                    'Foo' => null,
+                    'Bar' => true,
+                    'Baz' => 'false',
+                ],
+                'attr' => [
+                    'data-guidance-text' => 'foo <span class="bar">bar</span>'
+                ]
+            ))
+            ->getForm();
             
-    //     $this->compareOutput($form, 'choice-guidance.html.twig');
-    // }
+        $this->compareOutput($form, 'choice-guidance.html.twig');
+    }
 
-    // /**
-    //  * @group choice
-    //  */
-    // public function testChoiceFieldGuidanceContainer ()
-    // {
-    //     $form = $this->formFactory->createBuilder()
-    //         ->add('field', ChoiceType::class, array(
-    //             'label' => 'foo',
-    //             'required' => false,
-    //             'attr' => [
-    //                 'data-guidance-text' => 'foo <span class="bar">bar</span>',
-    //                 'data-guidance-container' => 'baz'
-    //             ]
-    //         ))
-    //         ->getForm();
+    /**
+     * @group choice
+     */
+    public function testChoiceFieldRadiosGuidanceContainer ()
+    {
+        $form = $this->formFactory->createBuilder()
+            ->add('field', ChoiceType::class, array(
+                'label' => 'foo',
+                'required' => false,
+                'expanded' => true,
+                'multiple' => false,
+                'choices'  => [
+                    'Foo' => null,
+                    'Bar' => true,
+                    'Baz' => 'false',
+                ],
+                'attr' => [
+                    'data-guidance-text' => 'foo <span class="bar">bar</span>',
+                    'data-guidance-container' => 'baz'
+                ]
+            ))
+            ->getForm();
             
-    //     $this->compareOutput($form, 'choice-guidance-container.html.twig');
-    // }
+        $this->compareOutput($form, 'choice-guidance-container.html.twig');
+    }
 
-    // /**
-    //  * @group choice
-    //  */
-    // public function testChoiceFieldRequiredGuidanceText ()
-    // {
-    //     $form = $this->formFactory->createBuilder()
-    //         ->add('field', ChoiceType::class, array(
-    //             'label' => 'foo',
-    //             'attr' => [
-    //                 'data-guidance-text' => 'foo <span class="bar">bar</span>'
-    //             ]
-    //         ))
-    //         ->getForm();
+    /**
+     * @group choice
+     */
+    public function testChoiceFieldRadiosRequiredGuidance ()
+    {
+        $form = $this->formFactory->createBuilder()
+            ->add('field', ChoiceType::class, array(
+                'label' => 'foo',
+                'expanded' => true,
+                'multiple' => false,
+                'choices'  => [
+                    'Foo' => null,
+                    'Bar' => true,
+                    'Baz' => 'false',
+                ],
+                'attr' => [
+                    'data-guidance-text' => 'foo <span class="bar">bar</span>'
+                ]
+            ))
+            ->getForm();
             
-    //     $this->compareOutput($form, 'choice-required-guidance.html.twig');
-    // }
-
-    // /**
-    //  * @group choice
-    //  */
-    // public function testChoiceFieldPrependText ()
-    // {
-    //     $form = $this->formFactory->createBuilder()
-    //         ->add('field', ChoiceType::class, array(
-    //             'label' => 'foo',
-    //             'required' => false,
-    //             'attr' => [
-    //                 'data-prepend-text' => 'bar'
-    //             ]
-    //         ))
-    //         ->getForm(); 
-            
-    //     $this->compareOutput($form, 'choice-prepend.html.twig');
-    // }
-
-    // /**
-    //  * @group choice
-    //  */
-    // public function testChoiceFieldAppendText ()
-    // {
-    //     $form = $this->formFactory->createBuilder()
-    //         ->add('field', ChoiceType::class, array(
-    //             'label' => 'foo',
-    //             'required' => false,
-    //             'attr' => [
-    //                 'data-append-text' => 'bar'
-    //             ]
-    //         ))
-    //         ->getForm();
-            
-    //     $this->compareOutput($form, 'choice-append.html.twig');
-    // }
-
-    // /**
-    //  * @group choice
-    //  */
-    // public function testChoiceFieldPrependTextAppendText ()
-    // {
-    //     $form = $this->formFactory->createBuilder()
-    //         ->add('field', ChoiceType::class, array(
-    //             'label' => 'foo',
-    //             'required' => false,
-    //             'attr' => [
-    //                 'data-prepend-text' => 'bar',
-    //                 'data-append-text' => 'baz'
-    //             ]
-    //         ))
-    //         ->getForm();
-            
-    //     $this->compareOutput($form, 'choice-prepend-append.html.twig');
-    // }
+        $this->compareOutput($form, 'choice-required-guidance.html.twig');
+    }
 }
