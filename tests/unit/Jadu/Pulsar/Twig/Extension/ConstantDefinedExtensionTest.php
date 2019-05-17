f411e2cac890f4f9acf0cff4ce963ba1ba3a5440
Paul Stanton <paul@coffeepowered.co.uk>
2019-05-17 15:22:40 +0100

    Add missing constant defined extention test

--- tests/unit/Jadu/Pulsar/Twig/Extension/ConstantDefinedExtensionTest.php
+++ tests/unit/Jadu/Pulsar/Twig/Extension/ConstantDefinedExtensionTest.php
@@ -10,10 +10,24 @@ class ConstantDefinedExtensionTest extends \PHPUnit\Framework\TestCase
     public function setUp()
     {
         $this->ext = new ConstantDefinedExtension();
+
+        define('FOO', true);
     }
 
     public function testGetName()
     {
         $this->assertEquals('constant_defined', $this->ext->getName());
+    }
+
+    public function testConstantDefinedFunction()
+    {
+        $loader = new Twig_Loader_Array(array(
+            'index.html' => '{% if constant_defined("FOO") %}true{% endif %}',
+        ));
+
+        $twig = new Twig_Environment($loader);
+        $twig->addExtension(new ConstantDefinedExtension());
+
+        $this->assertEquals('true', $twig->render('index.html'));
     }
 }
