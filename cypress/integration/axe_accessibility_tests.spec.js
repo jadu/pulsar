6548dea11b5702a4e2d3b14a7650c2a7b8593c6c
Paul Stanton <paul@coffeepowered.co.uk>
2019-12-04 14:45:22 +0000

    Wording tweak for cypress axe runner

--- cypress/integration/axe_accessibility_tests.spec.js
+++ cypress/integration/axe_accessibility_tests.spec.js
@@ -1,4 +1,4 @@
-describe('Test pages for accessibility issues', function() {
+describe('The following pages should not throw a11y errors in aXe: ', function() {
     it('Lexicon homepage', () => {
         cy.visit('http://localhost:9000/app/app.php/lexicon')
         cy.injectAxe()
