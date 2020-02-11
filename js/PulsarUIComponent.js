64e5c2411e1dba8fd5b6fdecd78c30ec02d98cc2
Paul Stanton <paul@coffeepowered.co.uk>
2020-02-11 13:21:57 +0000

    Restore ability to switch off datatable row selection with a data attribute
    This had broken somewhat when the new centralised options ability was added.

--- js/PulsarUIComponent.js
+++ js/PulsarUIComponent.js
@@ -55,7 +55,22 @@ PulsarUIComponent.getDatatableOptions = function ($table) {
             className: 'dt-row-selected',
             style: 'multi',
             selector: 'td.table-selection'
-        };
+        },
+        columnDefs = [
+            {
+                className: 'control',
+                orderable: false,
+                targets: 0
+            },
+            {
+                searchable: false,
+                targets: [0]
+            },
+            {
+                orderable: false,
+                targets: [0, 1]
+            }
+        ];
 
     if ($table.length && $table.data('empty-table')) {
         langEmptyTable = $table.data('empty-table');
@@ -70,8 +85,8 @@ PulsarUIComponent.getDatatableOptions = function ($table) {
     }
 
     if ($table.length && $table.data('select') === false) {
-        dom = '<"dataTables_top"irf><"dataTables_actions"T><"dt-disable-selection"t><"dataTables_bottom"pl>';
         select = false;
+        columnDefs = [];
     }
 
     const options = {
@@ -79,21 +94,7 @@ PulsarUIComponent.getDatatableOptions = function ($table) {
         autoWidth: false,
         buttons: [],
         className: 'dt-row-selected',
-        columnDefs: [
-            {
-                className: 'control',
-                orderable: false,
-                targets: 0
-            },
-            {
-                searchable: false,
-                targets: [0]
-            },
-            {
-                orderable: false,
-                targets: [0, 1]
-            }
-        ],
+        columnDefs: columnDefs,
         initComplete: initComplete,
         drawCallback: drawCallback,
         dom: dom,
