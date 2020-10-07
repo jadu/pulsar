1328944c13c32d8d13e780118944a3d411577092
Paul Stanton <paul@coffeepowered.co.uk>
2020-10-07 14:38:57 +0100

    Try all character wildcard search

--- js/DropZone/DropZoneValidationUtils.js
+++ js/DropZone/DropZoneValidationUtils.js
@@ -23,7 +23,7 @@ class DropZoneValidationUtils {
                     } else if (mime.indexOf('*') >= 0) {
                         // if the user has specified a wildcard mime e.g. 'image/*'
                         // we will create a wildcard expression and test against it
-                        const re = new RegExp(mime.replace('*', '[a-z\W]*'), 'g');
+                        const re = new RegExp(mime.replace('*', '.*'));
 
                         valid = !!(re.exec(type));
                     } else {
