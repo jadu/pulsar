45ef189504052745016e28c463c957fe2108a5d1
Paul Stanton <paul@coffeepowered.co.uk>
2020-10-16 15:58:56 +0100

    Move htmLawed filter config to private constant

--- src/Jadu/Pulsar/Twig/Extension/SanitiseExtension.php
+++ src/Jadu/Pulsar/Twig/Extension/SanitiseExtension.php
@@ -19,18 +19,18 @@ class SanitiseExtension extends \Twig_Extension
         );
     }
 
+    private const FILTER_CONFIG = array(
+        'comments' => 0,
+        'cdata' => 0,
+        'deny_attribute' => 'on*',
+        'unique_ids' => 0,
+        'elements' => '* -applet -audio -canvas -embed -iframe -object -script -video',
+        'schemes' => 'href: aim, feed, file, ftp, gopher, http, https, irc, mailto, news, nntp, sftp, ssh, tel, telnet; style: !; *:file, http, https'
+    );
+
     public function sanitise($data)
     {
-        $config = array(
-            'comments' => 0,
-            'cdata' => 0,
-            'deny_attribute' => 'on*',
-            'unique_ids' => 0,
-            'elements' => '* -applet -audio -canvas -embed -iframe -object -script -video',
-            'schemes' => 'href: aim, feed, file, ftp, gopher, http, https, irc, mailto, news, nntp, sftp, ssh, tel, telnet; style: !; *:file, http, https'
-        );
-
-        return Htmlawed::filter($data, $config);
+        return Htmlawed::filter($data, self::FILTER_CONFIG);
     }
 
 }
