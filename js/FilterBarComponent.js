5cf18d06d3ee4f18baeb263306e5431d0706e1fc
Paul Stanton <paul@coffeepowered.co.uk>
2022-04-11 14:34:11 +0100

    Turn filter bar add button to outline button for more contrast

--- js/FilterBarComponent.js
+++ js/FilterBarComponent.js
@@ -64,7 +64,7 @@ function createFilterListButton ($filterbar) {
     });
 
     // Build up list and mark up for Add filter button
-    $addFilterButton = $('<button class="btn filter-bar__add" type="button" aria-expanded="false" data-ui="show-filter-list" data-toggle="popover" data-trigger="click" title="Filter by" data-html="true" data-placement="bottom" data-content="" aria-haspopup="true"><i class="icon-plus"><span class="hide">Add filter</span></i></button>');
+    $addFilterButton = $('<button class="btn filter-bar__add btn--outline" type="button" aria-expanded="false" data-ui="show-filter-list" data-toggle="popover" data-trigger="click" title="Filter by" data-html="true" data-placement="bottom" data-content="" aria-haspopup="true"><i class="icon-plus"><span class="hide">Add filter</span></i></button>');
     $addFilterButton.attr('data-content', $filterList[0].outerHTML);
 
     // Append button and label wrapper
