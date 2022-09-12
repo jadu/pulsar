861b5b673d5ed5d18769de90443839ea492e3bb8
Paul Stanton <paul@coffeepowered.co.uk>
2022-09-12 15:22:08 +0100

    Remove some docusaurus folders we may not need

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/paulstanton/Localhost/pulsar/docs/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/paulstanton/Localhost/pulsar/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/paulstanton/Localhost/pulsar/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/paulstanton/Localhost/pulsar/docs/src/css/custom.scss'),
-];
