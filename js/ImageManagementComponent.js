58c6ab3e29fb65b20ecc897f103a1dd7cd2b2994
Paul Stanton <paul@coffeepowered.co.uk>
2023-01-26 12:43:11 +0000

    Add interactions for image editor controls

--- js/ImageManagementComponent.js
+++ js/ImageManagementComponent.js
@@ -6,13 +6,18 @@ var $ = require('jquery');
 
 function ImageManagement(html) {
     this.$html = html;
+    this.cropper;
 }
 
 ImageManagement.prototype.init = function () {
-
-    this.$html.on('show.bs.modal', '#editModal', function () { 
+    
+    var component = this;
+    
+    component.$html.on('show.bs.modal', '#editModal', function () { 
         const image = document.getElementById('cropper-source');
-        const cropper = new Cropper(image, {
+        
+        component.cropper = new Cropper(image, {
+            viewMode: 1,
             autoCropArea: 1,
             crop(event) {
                 console.log(event.detail.x);
@@ -24,6 +29,97 @@ ImageManagement.prototype.init = function () {
                 console.log(event.detail.scaleY);
             },
         });
+    });
+    
+    component.$html.on('click', '[data-method]', function(event) {
+        var e = event || window.event;
+        
+        var target = e.target || e.srcElement;
+        var cropped;
+        var result;
+        var input;
+        var data;
+        
+        console.log(target.className.indexOf('radio'));
+        if (target.className.indexOf('radio') == -1) {
+            e.preventDefault();
+        }
+        
+        if (!component.cropper) {
+            return;
+        }
+        
+        while (target !== this) {
+            if (target.getAttribute('data-method')) {
+                break;
+            }
+            
+            target = target.parentNode;
+        }
+        
+        
+        if (target.disabled || target.className.indexOf('disabled') > -1) {
+            return;
+        }
+        
+        data = {
+            method: target.getAttribute('data-method'),
+            target: target.getAttribute('data-target'),
+            option: target.getAttribute('data-option') || undefined,
+            secondOption: target.getAttribute('data-second-option') || undefined
+        };
+        
+        cropped = component.cropper.cropped;
+        
+        if (data.method) {
+            if (typeof data.target !== 'undefined') {
+                input = document.querySelector(data.target);
+                
+                if (!target.hasAttribute('data-option') && data.target && input) {
+                    try {
+                        data.option = JSON.parse(input.value);
+                    } catch (e) {
+                        console.log(e.message);
+                    }
+                }
+            }
+            
+            switch (data.method) {
+                case 'rotate':
+                if (cropped && component.cropper.options.viewMode > 0) {
+                    component.cropper.clear();
+                }
+                
+                break;
+                
+                case 'getCroppedCanvas':
+                try {
+                    data.option = JSON.parse(data.option);
+                } catch (e) {
+                    console.log(e.message);
+                }
+                
+                if (uploadedImageType === 'image/jpeg') {
+                    if (!data.option) {
+                        data.option = {};
+                    }
+                    
+                    data.option.fillColor = '#fff';
+                }
+                
+                break;
+            }
+            
+            result = component.cropper[data.method](data.option, data.secondOption);
+            
+            if (typeof result === 'object' && result !== component.cropper && input) {
+                try {
+                    input.value = JSON.stringify(result);
+                } catch (e) {
+                    console.log(e.message);
+                }
+            }
+        }
     });
 };
 
